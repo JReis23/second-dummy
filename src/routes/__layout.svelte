46d0ad59fa6aeab1c92bf967c4bf2fe20f84988f
JReis23 <cjfreis23@gmail.com>
2021-12-13 22:04:05 +0100

    add cdn fontawesome

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,20 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link
+		href="https://cdn.jsdelivr.net/npm/daisyui@1.16.2/dist/themes.css"
+		rel="stylesheet"
+		type="text/css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"
+	/>
+</svelte:head>
+
 <slot />
