900a09709ba2a80255bee54f1f1ac5423f8351f0
JReis23 <cjfreis23@gmail.com>
2021-12-13 12:06:25 +0100

    chengement de favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,15 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link
+		href="https://cdn.jsdelivr.net/npm/daisyui@1.16.2/dist/themes.css"
+		rel="stylesheet"
+		type="text/css"
+	/>
+	<script defer src="../node_modules/@fortawesome/fontawesome-free/js/brands.js"></script>
+	<script defer src="../node_modules/@fortawesome/fontawesome-free/js/solid.js"></script>
+	<script defer src="../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"></script>
+</svelte:head>
+
 <slot />
